ade51302ba3835e1d2974174ab0c5c443a187d44
isayme <isaymeorg@gmail.com>
2017-01-03 14:36:21 +0800

    add test

--- test/adapters/redis.js
+++ test/adapters/redis.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const expect = require('chai').expect
+const sinon = require('sinon')
 const Redis = require('../../lib/').Adapters.redis
 const redis = require('redis')
 
@@ -21,6 +22,48 @@ describe(`redis`, function () {
     it('should throw opts.prefix required', function () {
       let fn = () => new Redis({redis: client, throttle: 2})
       expect(fn).throw(Error, 'opts.prefix required')
+    })
+  })
+
+  describe('error', function () {
+    let adapter = new Redis({
+      redis: client,
+      throttle: 1,
+      prefix: 'dfrl:test'
+    })
+
+    it('should setnx should rejected when redis.eval have error', function * () {
+      let stub = sinon.stub(adapter.redis, 'eval', function () {
+        let callback = arguments[arguments.length - 1]
+
+        callback(new Error('eval failed'))
+      })
+
+      let spy = sinon.spy()
+
+      yield adapter.setnx('id', 1).catch(spy)
+
+      expect(spy.callCount).equal(1)
+      expect(spy.args[0][0].message).eql('eval failed')
+
+      stub.restore()
+    })
+
+    it('should clear should rejected when redis.del have error', function * () {
+      let stub = sinon.stub(adapter.redis, 'del', function () {
+        let callback = arguments[arguments.length - 1]
+
+        callback(new Error('del failed'))
+      })
+
+      let spy = sinon.spy()
+
+      yield adapter.clear('id', 1).catch(spy)
+
+      expect(spy.callCount).equal(1)
+      expect(spy.args[0][0].message).eql('del failed')
+
+      stub.restore()
     })
   })
 })
