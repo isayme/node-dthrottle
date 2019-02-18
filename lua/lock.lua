33495ccb8cca67eaf145f7aa629a69723b353cd3
iSayme <isaymeorg@gmail.com>
2019-02-18 09:41:55 +0800

    处理阿里云对KEYS的限制
    https://www.alibabacloud.com/help/zh/doc-detail/26356.htm

--- lua/lock.lua
+++ lua/lock.lua
@@ -1,10 +1,9 @@
-local KEY = KEYS[1]
 local timestamp = tonumber(ARGV[1])
 local expire = tonumber(ARGV[2])
 
-local res = redis.call('setnx', KEY, timestamp)
+local res = redis.call('setnx', KEYS[1], timestamp)
 if res > 0 then
-  redis.call('expire', KEY, expire)
+  redis.call('expire', KEYS[1], expire)
   return 1
 else
   return 0
